52a5417cba3f035b8ef03b0cd98be4d24858092c
Damián López <lopezrunco@gmail.com>
2023-07-05 21:16:10 +0530

    pagination deleted in preoffers list

--- src/controllers/preoffer/get-all.js
+++ src/controllers/preoffer/get-all.js
@@ -1,22 +1,8 @@
-const mongoose = require('mongoose')
 const { preofferModel } = require('../../models/preoffer')
 
 module.exports = (request, response) => {
-    const lotId = request.body.lotId
-
-    const pagination = {
-        offset: 0,
-        limit: 10
-    }
-    if (request.query.page && request.query.itemsPerPage) {
-        pagination.offset = (request.query.page - 1) * request.query.itemsPerPage,
-            pagination.limit = parseInt(request.query.itemsPerPage)
-    }
-
     preofferModel
         .find({ lotId: request.body.lotId })
-        .skip(pagination.offset)
-        .limit(pagination.limit)
         .then(preoffers => {
             preofferModel
                 .count()
@@ -24,14 +10,12 @@ module.exports = (request, response) => {
                     const meta = {
                         count
                     }
-
                     response.status(200).json({
                         meta,
                         preoffers
                     })
                 }).catch(error => {
                     console.error(error)
-
                     response.status(500).json({
                         message: 'Error trying to list the preoffers'
                     })
