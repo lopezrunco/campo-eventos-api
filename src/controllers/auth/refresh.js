f4fd7401eaa9e0bbc57fa6afe9ab02b98157b398
lopezrunco <lopezrunco@gmail.com>
2024-01-18 14:46:40 +0530

    refresh token controller test passed

--- src/controllers/auth/refresh.js
+++ src/controllers/auth/refresh.js
@@ -3,13 +3,10 @@ const { REFRESH_TOKEN_TYPE, CONSUMER_TOKEN_TYPE } = require('../../utils/token-t
 
 module.exports = (request, response) => {
     if (request.token.type === 'REFRESH') {
-        const token = createToken(request.user, CONSUMER_TOKEN_TYPE, '30m') // Add the user token
-        const refreshToken = createToken(request.user, REFRESH_TOKEN_TYPE, '3d') // Add the user refresh token
-        // Return both tokens
-        response.json({
-            token,
-            refreshToken
-        })
+        // Add and return user token & refresh token
+        const token = createToken(request.user, CONSUMER_TOKEN_TYPE, '30m')
+        const refreshToken = createToken(request.user, REFRESH_TOKEN_TYPE, '3d')
+        response.json({ token, refreshToken })
     } else {
         response.status(401).end()
     }
